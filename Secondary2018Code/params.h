c225a7b829886434ca12acaedddaf6dd79048b20
ElieGrnr <elie.grenier@gmail.com>
2018-03-18 20:00:26 +0000

    [PINS ASSIGNMENT] Adds pins params. First commit of secondary2018!

---
+++ Secondary2018Code/params.h
@@ -0,0 +1,36 @@
+/*
+ * params.h
+ *
+ *  Created on: 18 mars 2018
+ *      Author: elie
+ */
+
+#ifndef PARAMS_H_
+#define PARAMS_H_
+
+const int ENCODEUR1_A = 21;
+const int ENCODEUR1_B = 20;
+const int ENCODEUR2_A = 17;
+const int ENCODEUR2_B = 16;
+
+const int MOT1_PWM = 6;
+const int MOT1_DIR = 5;
+const int MOT2_PWM = 28;
+const int MOT2_DIR = 29;
+
+const int SERVO1 = 39;
+const int SERVO2 = 15;
+const int SERVO3 = 37;
+
+const int MOT_GALET_L = 35;
+const int MOT_GALET_R = 36;
+
+const int COLOR = 26;
+const int TIRETTE = 25;
+
+const int SPARE1 = 10;
+const int SPARE2 = 9;
+const int SPARE3 = 8;
+const int SPARE4 = 7;
+
+#endif /* PARAMS_H_ */
