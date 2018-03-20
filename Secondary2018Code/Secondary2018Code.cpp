2f02deda6ac8e0385b0e748f0e61b519fade2cc6
ElieGrnr <elie.grenier@gmail.com>
2018-03-20 20:31:31 +0000

    [SecondaryCode] Testing code for motor control.

--- Secondary2018Code/Secondary2018Code.cpp
+++ Secondary2018Code/Secondary2018Code.cpp
@@ -3,15 +3,18 @@
 #include "Metro.h"
 #include "params.h"
 #include "odometry.h"
+#include "motorControl.h"
 
 Metro controlTime = Metro((unsigned long)(CONTROL_PERIOD * 1000));
-
-
+Metro test = Metro(3000);
+int i=0;
+int tab[] = {50, 100, 0, -50, -100, 0};
 //The setup function is called once at startup of the sketch
 void setup()
 {
 	Serial.begin(115200);
 	Odometry::init();
+	MotorControl::init();
 }
 
 // The loop function is called in an endless loop
@@ -19,5 +22,10 @@ void loop()
 {
 	if(controlTime.check()) {
 		Odometry::update();
+		MotorControl::update();
+	}
+	if(test.check()) {
+		MotorControl::set_cons(tab[i%6], 0);
+		i++;
 	}
 }
